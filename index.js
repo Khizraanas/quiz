0cec50158ac242987be139031de32426bdf93988
Khizra Anas <165975743+Khizraanas@users.noreply.github.com>
2024-08-15 10:52:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+var username = prompt("What's your name?");
+alert("Welcome__" + username + "__to this Amazing quiz session!");
+alert("BEST OF LUCK!!!");
+
+//  Question #1
+
+var quiz1 = prompt("HTML stand for? A) Hyper text markup language, B) Home Tool Markup Language C) Hyperlink and Text Markup language   D)  Hyperlinking and Text Markup language");
+if (quiz1 === "A") {
+   alert(" Congratulation__! Your Answer is Correct____!!!");
+}
+else {
+   (quiz1 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #2
+
+var quiz2 = prompt(" How can you create an ordered list in HTML? a) <ul>. b) <ol> c) <list> d) <order>.");
+if (quiz2 === "b") {
+   alert(" Congratulation__! Your Answer is Correct____!!!");
+}
+else {
+   (quiz2 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #3
+
+var quiz3 = prompt("which css property is used to change the text color of an element? a) font-color. b) color. c) text-color. d) foreground-color.");
+if (quiz3 === "b") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz3 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #4
+
+var quiz4 = prompt(" What does the Symbol data type represent in JavaScript? a) A unique and immutable primitive value. b) A mutable object. c) A method to handle asynchronous operations. d) A data structure for handling sets of values.");
+if (quiz4 === "a") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz4 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+
+//  Question #5
+
+var quiz5 = prompt("Can you customize the appearance of the alert dialog box (like changing colors or fonts) using CSS? a) Yes, by using external stylesheets. b) Yes, by using inline styles. c) No, the appearance cannot be customized. d) Yes, by using JavaScript properties.");
+if (quiz5 === "c") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz5 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//  Question #6
+
+var quiz6 = prompt("Which of the following is the correct syntax for referring to an external script called 'script.js'?  a) <script href='script.js'> .b) <script name='script.js'> .c) <script src='script.js'> . d) <script file='script.js'>");
+if (quiz6 === "c") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz6 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//  Question #7
+
+var quiz7 = prompt("What is the purpose of the Set object in JavaScript? a) To store unique values of any type. b) To store key-value pairs. c) To create an ordered list of elements. d) To perform mathematical set operations.");
+if (quiz7 === "a") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz7 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+
+//  Question #8
+
+var quiz8 = prompt("What does DOM stand for? A) Document Object Model. B) Data Object Model. C) Document Oriented Model. D) Data Oriented Model");
+if (quiz8 === "A") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz8 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//Question #9
+
+var quiz9 = prompt("How do you write 'Hello,World!' in an alert box? A) alertbox('Hello World!'). B) ('Hello World!').msg C) alert('Hello World!'). D)msgbox ('Hello World!').");
+if (quiz9 === "c") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz9 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
+//Question #10
+
+var quiz10 = prompt("How can you add a comment in javascript? A) <!--This is comment-->. B) # This is a comment C) /*This is comment*/ D) //This is a comment");
+if (quiz10 === "d") {
+   alert("Congratulation__! Your Answer is correct___!!!");
+}
+else {
+   (quiz10 === "Anything else would be wrong");
+   alert("Incorrect__! Best of luck next time____!!!");
+}
